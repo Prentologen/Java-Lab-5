ea3cf84f47b24b89a6a656c4a99ee86660433107
Prentologen <dankababak@gmail.com>
2024-02-21 20:56:39 +0200

    Download

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class A{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int counter = 0;
+
+        boolean pin_code_coincided = false;
+
+        while (counter != 5){
+
+            String correct = sc.nextLine();
+
+            String vasya = sc.nextLine();
+
+            counter++;
+
+            if(correct.equals(vasya)){
+
+                System.out.println("СORRECT");
+
+                pin_code_coincided = true;
+
+                break;
+            }
+
+            System.out.println("INCORRECT " + counter);
+        }
+
+        if (!pin_code_coincided){
+            
+            System.out.println("Error");
+        }
+    }
+}
